62a6388ce3cfe1a33432e9c0f3a70c79e2a86a04
Stefano Calzoni <stefanoclz@hotmail.it>
2022-07-13 14:59:34 +0200

    Generata lista pizze raggiungibile da index tramite link

---
+++ Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public string name = "";
+        public string description = "";
+        public string fotoLink = "";
+        public decimal prezzo;
+
+        public Pizza(string name, string description, string fotoLink, decimal prezzo)
+        {
+            this.name = name;
+            this.description = description;
+            this.fotoLink = fotoLink;
+            this.prezzo = prezzo;
+        }
+
+    }
+}
